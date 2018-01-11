bd7df39c88a8b94c54e3ef3402958c5f33970046
whasf <whasf@dspt.info>
2018-01-10 22:01:51 -0600

    Added logging Fixed deferral to exit program (it's not immediate)

--- CatFeeders/StartupTask.cs
+++ CatFeeders/StartupTask.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
 using Windows.System.Threading;
+using System.IO;
+
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -15,21 +17,37 @@ namespace CatFeeders
     {
         private BackgroundTaskDeferral deferral;
         GpioController gpio = GpioController.GetDefault();
-        int[] pinNumbers = new int[] { 17, 27 , 22 };
+        int[] pinNumbers = new int[] { 17, 27, 22 };
         GpioPin[] pins = new GpioPin[3];
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             deferral = taskInstance.GetDeferral();
+            WriteLog("Starting");
             foreach (int i in pinNumbers)
             {
                 using (GpioPin p = gpio.OpenPin(i))
                 {
                     p.Write(GpioPinValue.Low);
                     p.SetDriveMode(GpioPinDriveMode.Output);
-                    System.Threading.Tasks.Task.Delay(5000).Wait();
+                    WriteLog("Ran pin " + p.PinNumber.ToString());
+                    System.Threading.Tasks.Task.Delay(4000).Wait();
                 }
             }
+            WriteLog("Finished");
+            System.Threading.Tasks.Task.Delay(4000).Wait();
+            deferral.Complete();
+        }
+        private static string GetNow()
+        {
+            string datePatt = @"M/d/yyyy hh:mm:ss tt";
+            DateTime currtime = DateTime.Now;
+            string now = currtime.ToString(datePatt);
+            return now;
+        }
+        private void WriteLog(string message)
+        {
+            File.AppendAllText("C:\\logs\\feeder.log", GetNow()+" : "+message + Environment.NewLine);
         }
     }
 }
